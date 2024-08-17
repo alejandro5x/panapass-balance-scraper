a3c8079a59c9bf11012aaf4ba6f2e727bf42e12e
Alejandro Guerra <57050425+alejandro5x@users.noreply.github.com>
2024-08-16 20:27:43 -0500

    Minor changes

--- panapass-balance-scraper.py
+++ panapass-balance-scraper.py
@@ -10,13 +10,14 @@ import json
 
 load_dotenv()
 def run(playwright: Playwright) -> None:
-    url = os.getenv('URL_PANAPASS')
-    usuario = os.getenv('USUARIO_PANAPASS')
-    contrasena = os.getenv('CONTRASENA_PANAPASS')
+    usuario = os.getenv('PANAPASS_NUMBER')
+    contrasena = os.getenv('PANAPASS_PASSWORD')
     mqtt_user = os.getenv('MQTT_USER')
     mqtt_password = os.getenv('MQTT_PASSWORD')
-    mqtt_server = os.getenv('MQTT_SERVER')
+    mqtt_server = os.getenv('MQTT_BROKER')
     mqtt_port = int(os.getenv('MQTT_PORT'))
+    mqtt_topic = os.getenv('MQTT_TOPIC')
+
     
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
@@ -41,9 +42,9 @@ def run(playwright: Playwright) -> None:
     client.username_pw_set(mqtt_user, mqtt_password)
     client.connect(mqtt_server, mqtt_port)
     client.loop_start()
-    
     xpayload = json.dumps({"state": str(monto), "updated_ts": str(int(time.time())), "updated_dt": str(datetime.now()) }, sort_keys=True, default=str)
-    client.publish(topic="homeassistant/sensor/panapass", payload=xpayload, qos=0, retain=False)
+    client.publish(topic=mqtt_topic, payload=xpayload, qos=0, retain=False)
+    
     time.sleep(delay)
 
 with sync_playwright() as playwright:
